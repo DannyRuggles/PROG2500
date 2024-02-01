1a8fa3a53d2631d7ae2924d95255efcce513be25
DannyRuggles <dannyruggles@hotmail.com>
2024-02-01 13:17:28 -0400

    Uploaded GitConfig Prototype

---
+++ Git Config/GitConfigEditor/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GitConfigEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DisplayCurrentGitConfig();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SetGitConfig("user.name", txtUsername.Text);
+            SetGitConfig("user.email", txtEmail.Text);
+        }
+
+        private void SetGitConfig(string key, string value)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("git")
+            {
+                Arguments = $"config --global {key} \"{value}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Process proc = new Process() { StartInfo = startInfo };
+            proc.Start();
+            proc.WaitForExit();
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        private string GetGitConfig(string key)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("git")
+            {
+                Arguments = $"config --global --get {key}",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+            Process proc = new Process() { StartInfo = startInfo };
+            proc.Start();
+            string result = proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+            return result.Trim();
+        }
+
+        private void DisplayCurrentGitConfig()
+        {
+            txtUsername.Text = GetGitConfig("user.name");
+            txtEmail.Text = GetGitConfig("user.email");
+        }
+    }
+}
